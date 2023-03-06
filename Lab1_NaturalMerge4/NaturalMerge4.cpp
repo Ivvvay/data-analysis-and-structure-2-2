292fc9eca67f78740bf58a4c6f4d23dda9c95b8a
Lepeshkin Ivan <ivvvay@gmail.com>
2023-03-06 18:21:38 +0700

    Create bool isFileContainsSortedArray(...)

--- Lab1_NaturalMerge4/NaturalMerge4.cpp
+++ Lab1_NaturalMerge4/NaturalMerge4.cpp
@@ -3,6 +3,7 @@
 #include <random>
 
 bool createFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue);
+bool isFileContainsSortedArray(const std::string& fileName);
 
 
 bool createFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue) {
@@ -16,6 +17,26 @@ bool createFileWithRandomNumbers(const std::string& fileName, const int numbersC
     for (int i = 0; i < numbersCount; i++) {
         int numders = gen() % maxNumberValue;
         file << numders << " ";
+    }
+
+    file.close();
+    return true;
+}
+
+bool isFileContainsSortedArray(const std::string& fileName) {
+    std::ifstream file(fileName);
+    if (!file.is_open())
+        return false;
+
+    int num1, num2;
+    file >> num1;
+    while(!file.eof()) {
+        file >> num2;
+        if (num1 > num2) {
+            file.close();
+            return false;
+        }
+        num1 = num2;
     }
 
     file.close();
