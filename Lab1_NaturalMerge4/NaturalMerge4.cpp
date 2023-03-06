6bdb90390d3a0614520fab5219175d95c192bdd4
Lepeshkin Ivan <ivvvay@gmail.com>
2023-03-06 18:20:42 +0700

    Create bool createFileWithRandomNumbers(...)

--- Lab1_NaturalMerge4/NaturalMerge4.cpp
+++ Lab1_NaturalMerge4/NaturalMerge4.cpp
@@ -1,3 +1,23 @@
-//
-// Created by Иван Лепёшкин on 06.03.2023.
-//
+#include <iostream>
+#include <fstream>
+#include <random>
+
+bool createFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue);
+
+
+bool createFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::ofstream file(fileName);
+
+    if(!file.is_open())
+        return false;
+
+    for (int i = 0; i < numbersCount; i++) {
+        int numders = gen() % maxNumberValue;
+        file << numders << " ";
+    }
+
+    file.close();
+    return true;
+}
