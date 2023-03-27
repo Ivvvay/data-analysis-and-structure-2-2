0378c2153605ece99799fdf6aa921795461530d7
Lepeshkin Ivan <ivvvay@gmail.com>
2023-03-28 04:19:57 +0700

    Create class Node + methods

--- Lab2_BinaryTree/BinaryTree.cpp
+++ Lab2_BinaryTree/BinaryTree.cpp
@@ -1,1 +1,37 @@
 #include "BinaryTree.h"
+
+BinaryTree::Node::Node(int key, Node *leftChild, Node *rightChild) :
+        _key(key), _leftChild(leftChild), _rightChild(rightChild) {}
+
+int BinaryTree::Node::getKey() const {
+    return _key;
+}
+
+void BinaryTree::Node::setKey(int key) {
+    _key = key;
+}
+
+int BinaryTree::Node::getNumChildren() const {
+    int count = 0;
+    if (_leftChild)
+        count++;
+    if (_rightChild)
+        count++;
+    return count;
+}
+
+BinaryTree::Node *BinaryTree::Node::getLeftChild() const {
+    return _leftChild;
+}
+
+BinaryTree::Node *BinaryTree::Node::getRightChild() const {
+    return _rightChild;
+}
+
+void BinaryTree::Node::setLeftChild(Node *leftChild) {
+    _leftChild = leftChild;
+}
+
+void BinaryTree::Node::setRightChild(Node *rightChild) {
+    _rightChild = rightChild;
+}
--- Lab2_BinaryTree/BinaryTree.h
+++ Lab2_BinaryTree/BinaryTree.h
@@ -3,6 +3,23 @@
 
 
 class BinaryTree {
+public:
+    class Node {
+    private:
+        int _key = 0;
+        Node* _leftChild = nullptr;
+        Node* _rightChild = nullptr;
+    public:
+        Node(int key = 0, Node* leftChild = nullptr, Node* rightChild = nullptr); //- конструкторы (по умолчанию и с параметрами);
+        ~Node() = default;
+        int getKey() const; //- получение ключа узла (целое число);
+        void setKey(int key);
+        int getNumChildren() const; //- получение количества прямых потомков узла;
+        Node* getLeftChild() const;
+        Node* getRightChild() const;
+        void setLeftChild(Node* leftChild);
+        void setRightChild(Node* rightChild);
+    };
 
 };
 
