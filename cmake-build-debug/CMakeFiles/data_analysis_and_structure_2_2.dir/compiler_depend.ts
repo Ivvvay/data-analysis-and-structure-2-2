3beaa21b17344451eaa426baf2b8e3d63d20f750
Lepeshkin Ivan <ivvvay@gmail.com>
2023-03-28 13:19:35 +0700

    Removed trash files

--- cmake-build-debug/CMakeFiles/data_analysis_and_structure_2_2.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for data_analysis_and_structure_2_2.
--- cmake-build-debug/CMakeFiles/data_analysis_and_structure_2_2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for data_analysis_and_structure_2_2.
-# This may be replaced when dependencies are built.
