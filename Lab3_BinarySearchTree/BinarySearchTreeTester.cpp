22d613cff63c4ef0885cadaa57d9049360b65e66
Lepeshkin Ivan <ivvvay@gmail.com>
2023-04-03 19:55:50 +0700

    create test getMinMaxKey and remove

--- Lab3_BinarySearchTree/BinarySearchTreeTester.cpp
+++ Lab3_BinarySearchTree/BinarySearchTreeTester.cpp
@@ -54,3 +54,72 @@ void BinarySearchTreeTester::copySubtreeSearch() {
 void BinarySearchTreeTester::check_copySubtreeSearch(BinarySearchTree &subTree, BinarySearchTree &tree, const int key) {
     assert(subTree.getHeight() + key == tree.getHeight());
 }
+
+void BinarySearchTreeTester::getMinMaxKey() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(0, _maxSize);
+
+    BinarySearchTree tree;
+    for (int i = 0; i <= _maxSize; i++) {
+        int key = dis(gen);
+        tree.addNode(key);
+    }
+
+    int min = tree.getMinKey();
+    int max = tree.getMaxKey();
+
+    check_getMinMaxKey(tree, min, max);
+}
+
+void BinarySearchTreeTester::check_getMinMaxKey(BinarySearchTree &tree, int min, int max) {
+    std::vector<int> keys = tree.getKeys();
+    auto min_element = std::min_element(keys.begin(), keys.end());
+    auto max_element = std::max_element(keys.begin(), keys.end());
+
+    assert(min == *min_element && max == *max_element);
+}
+
+void BinarySearchTreeTester::remove() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    int invalidKey = -1;
+    std::vector<int> nodeKeys;
+
+    BinarySearchTree tree;
+    tree.removeNodeSearch(invalidKey);
+
+    for (int i = 0 ; i < _maxSize; ++i) {
+        nodeKeys.push_back(i);
+        tree.addNode(i);
+    }
+    //std::cout << "tree.getSize()=" << tree.getSize() << ", nodeKeys.size()=" << nodeKeys.size() << std::endl;
+    //tree.printTree();
+
+    while (!nodeKeys.empty()) {
+        std::uniform_int_distribution<int> distribution(0, nodeKeys.size() - 1);
+        int removedNodeIndex = distribution(gen);
+        int removeNodeKey = nodeKeys[removedNodeIndex];
+        //std::cout << "--------" << removeNodeKey << "--------" << std::endl;
+
+        tree.removeNodeSearch(removeNodeKey);
+        if (removedNodeIndex == 0 && tree.getSize() == 0)
+            nodeKeys.clear();
+        else
+            nodeKeys.erase(nodeKeys.begin() + removedNodeIndex);
+        //std::cout << "tree.getSize()=" << tree.getSize() << ", nodeKeys.size()=" << nodeKeys.size() << std::endl;
+        check_remove(tree, nodeKeys.size());
+
+        //tree.printTree();
+    }
+    /*std::cout << "----------------" << std::endl;
+    tree.printTree();*/
+
+    tree.removeNodeSearch(invalidKey);
+    check_remove(tree, nodeKeys.size());
+}
+
+void BinarySearchTreeTester::check_remove(const BinaryTree &tree, const int size) {
+    assert(tree.getSize() == size);
+}
--- Lab3_BinarySearchTree/BinarySearchTreeTester.h
+++ Lab3_BinarySearchTree/BinarySearchTreeTester.h
@@ -10,11 +10,13 @@ public:
 protected:
     bool check_BST(BinarySearchTree::Node* root, int minVal, int maxVal);
     void check_copySubtreeSearch(BinarySearchTree &subTree, BinarySearchTree &tree, const int key);
-
+    void check_getMinMaxKey(BinarySearchTree &tree, int min, int max);
+    void check_remove(const BinaryTree &tree, const int size);
 private:
     void isBST();
     void copySubtreeSearch();
-
+    void getMinMaxKey();
+    void remove();
 private:
     int _maxSize;
 };
