e74b9717762456367807ce5de5618f99505a955c
Lepeshkin Ivan <ivvvay@gmail.com>
2023-05-01 20:06:18 +0700

    add new project file AVLtree

---
+++ Lab3_4_BinarySearchTree-AVL/AVLtree.h
@@ -0,0 +1,10 @@
+#ifndef LAB3_4_BINARYSEARCHTREE_AVL_AVLTREE_H
+#define LAB3_4_BINARYSEARCHTREE_AVL_AVLTREE_H
+#include "BinarySearchTree.h"
+
+class AVLtree : public BinarySearchTree {
+
+};
+
+
+#endif //LAB3_4_BINARYSEARCHTREE_AVL_AVLTREE_H
