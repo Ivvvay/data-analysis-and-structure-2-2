d09a56758f53803b12d413f208874f4b7c000d80
Lepeshkin Ivan <ivvvay@gmail.com>
2023-06-05 20:34:41 +0700

    wrote constructors and operators for them

--- Lab5_HuffmanCoding/Set.cpp
+++ Lab5_HuffmanCoding/Set.cpp
@@ -1,1 +1,43 @@
 #include "Set.h"
+
+Set::Set() {
+    for (int i = 0; i < _setSize; i++)
+        _setArray[i] = (char) 0;
+}
+
+Set::Set(unsigned char symbol) {
+    for (int i = 0; i < _setSize; i++)
+        _setArray[i] = (unsigned char) 0;
+
+    int index = (int) symbol / 8;
+    int bit = (int) symbol % 8;
+    _setArray[index] |= 1 << (7 - bit);
+}
+
+Set::Set(const Set& other) {
+    for (int i = 0; i < _setSize; i++)
+        _setArray[i] = other._setArray[i];
+}
+
+Set& Set::operator=(const Set& other) {
+    if (this == &other)
+        return *this;
+
+    for (int i = 0; i < _setSize; i++)
+        _setArray[i] = other._setArray[i];
+
+    return *this;
+}
+
+Set Set::operator|(const Set& other) {
+    Set tmp(*this);
+    tmp |= other;
+    return tmp;
+}
+
+Set Set::operator|=(const Set& other) {
+    for (int i = 0; i < _setSize; i++)
+        _setArray[i] |= other._setArray[i];
+
+    return *this;
+}
--- Lab5_HuffmanCoding/Set.h
+++ Lab5_HuffmanCoding/Set.h
@@ -2,7 +2,19 @@
 #define LAB5_HUFFMANCODING_SET_H
 
 class Set {
+public:
+    Set();
+    Set(unsigned char symbol);
+    Set(const Set& other);
+    ~Set() = default;
 
+    Set& operator=(const Set& other);
+    Set operator|(const Set& other);
+    Set operator|=(const Set& other);
+
+private:
+    static const int _setSize = 32;
+    unsigned char _setArray[_setSize];
 };
 
 
